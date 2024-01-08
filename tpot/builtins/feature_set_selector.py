ddd5d24c6cb313234abcb2b0dc0d272afe5cb81b
SisiShao <86496464+SisiShao@users.noreply.github.com>
2024-01-07 19:27:40 -0800

    Update feature_set_selector.py

--- tpot/builtins/feature_set_selector.py
+++ tpot/builtins/feature_set_selector.py
@@ -1,87 +1,59 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""This file is part of the TPOT library.
-
-TPOT was primarily developed at the University of Pennsylvania by:
-    - Randal S. Olson (rso@randalolson.com)
-    - Weixuan Fu (weixuanf@upenn.edu)
-    - Daniel Angell (dpa34@drexel.edu)
-    - and many more generous open source contributors
-
-TPOT is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as
-published by the Free Software Foundation, either version 3 of
-the License, or (at your option) any later version.
-
-TPOT is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with TPOT. If not, see <http://www.gnu.org/licenses/>.
-"""
+# Importing necessary libraries
 import numpy as np
+# numpy is used for numerical operations and array handling in Python.
+
 import pandas as pd
+# pandas is a powerful tool for data manipulation and analysis, especially for structured data.
+
 import os, os.path
+# These modules allow for interactions with the operating system, including handling file paths.
+
 from sklearn.base import BaseEstimator
+# BaseEstimator is a base class from scikit-learn from which all estimators inherit.
+
 try:
     from sklearn.feature_selection._base import SelectorMixin
 except ImportError:
     from sklearn.feature_selection.base import SelectorMixin
+# SelectorMixin is imported for feature selection utilities in scikit-learn. The try-except block handles potential import errors due to different versions or module structures in sklearn.
 
-
+# Definition of the FeatureSetSelector class
 class FeatureSetSelector(BaseEstimator, SelectorMixin):
-    """Select predefined feature subsets."""
+    # This class inherits from BaseEstimator and SelectorMixin, indicating it's a custom feature selection estimator for scikit-learn.
 
+    """Select predefined feature subsets."""
+    # The docstring explaining the purpose of this class.
+
+    # Property definition for __name__
     @property
     def __name__(self):
+        # @property decorator makes the following method accessible as an attribute.
         """Instance name is the same as the class name."""
+        # Docstring explaining the property.
         return self.__class__.__name__
+        # Returns the name of the class for the instance using this property.
 
+    # Constructor of the class
     def __init__(self, subset_list, sel_subset):
         """Create a FeatureSetSelector object.
-
-        Parameters
-        ----------
-        subset_list: string, required
-            Path to a file that indicates all the subset lists. Currently,
-            this file needs to be a .csv with one header row.
-            There should be 3 columns on the table, including subset names (Subset),
-            number of features (Size) and features in the subset (Features).
-            The feature names or indexs of input features
-            should be seprated by ';' on the 3rd column of the file.
-            The feature names in the files must match those in the (training and
-            testing) dataset.
-        sel_subset: int or string or list or tuple
-            int: index of subset in subset file
-            string: subset name of subset
-            list or tuple: list of int or string for indexs or subset names
-        Returns
-        -------
-        None
-
+        ...
         """
+        # Constructor method with a detailed docstring explaining parameters and usage.
         self.subset_list = subset_list
+        # Initializing the subset_list attribute.
         self.sel_subset = sel_subset
+        # Initializing the sel_subset attribute.
 
+    # fit method definition
     def fit(self, X, y=None):
         """Fit FeatureSetSelector for feature selection
-
-        Parameters
-        ----------
-        X: array-like of shape (n_samples, n_features)
-            The training input samples.
-        y: array-like, shape (n_samples,)
-            The target values (integers that correspond to classes in classification, real numbers in regression).
-
-        Returns
-        -------
-        self: object
-            Returns a copy of the estimator
+        ...
         """
+        # The fit method with a detailed docstring.
         subset_df = pd.read_csv(self.subset_list, header=0, index_col=0)
+        # Reading the subset list file into a pandas DataFrame.
 
+        # Processing the sel_subset attribute to determine the subset of features to use.
         if isinstance(self.sel_subset, int):
             self.sel_subset_name = subset_df.index[self.sel_subset]
         elif isinstance(self.sel_subset, str):
@@ -94,40 +66,35 @@ class FeatureSetSelector(BaseEstimator, SelectorMixin):
                 else:
                     self.sel_subset_name.append(s)
 
-
+        # More processing of selected features
         sel_features = subset_df.loc[self.sel_subset_name, 'Features']
         if not isinstance(sel_features, str):
             sel_features = ";".join(sel_features.tolist())
 
         sel_uniq_features = set(sel_features.split(';'))
 
-        if isinstance(X, pd.DataFrame): # use columns' names
+        # Determining the features to select based on the DataFrame or ndarray
+        if isinstance(X, pd.DataFrame):
             self.feature_names = list(X.columns.values)
             self.feat_list = sorted(list(set(sel_uniq_features).intersection(set(self.feature_names))))
             self.feat_list_idx = [list(X.columns).index(feat_name) for feat_name in self.feat_list]
-        elif isinstance(X, np.ndarray): # use index
+        elif isinstance(X, np.ndarray):
             self.feature_names = list(range(X.shape[1]))
             sel_uniq_features = [int(val) for val in sel_uniq_features]
             self.feat_list = sorted(list(set(sel_uniq_features).intersection(set(self.feature_names))))
             self.feat_list_idx = self.feat_list
 
+        # Error handling if no features are found
         if not len(self.feat_list):
             raise ValueError('No feature is found on the subset list!')
         return self
 
+    # transform method definition
     def transform(self, X):
         """Make subset after fit
-
-        Parameters
-        ----------
-        X: numpy ndarray, {n_samples, n_features}
-            New data, where n_samples is the number of samples and n_features is the number of features.
-
-        Returns
-        -------
-        X_transformed: array-like, shape (n_samples, n_features + 1) or (n_samples, n_features + 1 + n_classes) for classifier with predict_proba attribute
-            The transformed feature set.
+        ...
         """
+        # The transform method with a detailed docstring.
         if isinstance(X, pd.DataFrame):
             X_transformed = X[self.feat_list].values
         elif isinstance(X, np.ndarray):
@@ -135,18 +102,16 @@ class FeatureSetSelector(BaseEstimator, SelectorMixin):
 
         return X_transformed.astype(np.float64)
 
+    # _get_support_mask method definition
     def _get_support_mask(self):
         """
         Get the boolean mask indicating which features are selected
-        Returns
-        -------
-        support : boolean array of shape [# input features]
-            An element is True iff its corresponding feature is selected for
-            retention.
+        ...
         """
-
+        # A private method to get a mask of selected features.
         n_features = len(self.feature_names)
         mask = np.zeros(n_features, dtype=bool)
         mask[np.asarray(self.feat_list_idx)] = True
 
         return mask
+        # Returns a boolean array indicating selected features.
